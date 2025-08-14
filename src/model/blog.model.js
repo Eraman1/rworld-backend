79c3172f1b8351f78ada5e5a727f0d286f20eeed
Eraman1 <er.aman614@gmail.com>
2025-08-15 02:22:36 +0530

    fix

--- src/model/blog.model.js
+++ src/model/blog.model.js
@@ -11,9 +11,12 @@ const BlogPostSchema = new mongoose.Schema(
     title: {
       type: String,
       required: true,
+      unique: true,
     },
     slug: {
       type: String,
+      required: true,
+      unique: true,
     },
     content: {
       type: String,
